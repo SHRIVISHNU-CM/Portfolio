cd48b8277a46bd4bca2c6a303ed7f1f90a7e98a4
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-05-18 00:23:34 +0530

    navbar head about

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import About from "./Components/About"
+import Head from "./Components/Head"
+import Navbar from "./Components/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <div  className=" h-screen ">
+      <Navbar />
+      <Head/>
+      <About/>
+
+      </div>
+    </>
+  )
+}
+
+export default App
