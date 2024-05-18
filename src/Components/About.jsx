866ae405c3f34c7829c4291ecafac6fa440775e3
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-05-18 18:44:20 +0530

    about section

---
+++ src/Components/About.jsx
@@ -0,0 +1,21 @@
+
+
+function About() {
+    return (
+        <>
+            <div className="flex justify-around items-center">
+                <div className=" shadow-lg">
+                    <h1 className="card-title text-[25px]">About ME</h1>
+                </div>
+                <div className="w-[300px]">
+                    <p className="">
+                        I'm skilled in HTML,CSS,Tailwind CSS,DaisyUI,Javascript,React.js,Node.js,Express.js,MongoDB,SQL,and Python
+                    </p>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default About
