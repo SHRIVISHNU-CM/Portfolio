468d915d5e703f51ba0495aa3cbdc92d49bdb3eb
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-05-19 22:10:46 +0530

    framer motion

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,17 +3,14 @@ import { FaPhone } from "react-icons/fa6";
 function Navbar() {
     return (
         <>
-            <nav style={{backgroundColor:"#9599E2"}} className='navbar bg-base-300  rounded-2xl'>
+            <nav style={{backgroundColor:"#003153"}} className='navbar bg-base-300  rounded-2xl'>
                 <div className='navbar-start'>
-                    <h1 className='font-semibold'>Shri Vishnu C M</h1>
+                    <h1 className='font-semibold text-orange-600'>Shri Vishnu C M</h1>
                 </div>
                 <div className='navbar-end'>
-                    <h1>Personal Portfolio</h1>
+                    <h1 className='text-white'>Personal Portfolio</h1>
                 </div>
             </nav>
-            <div className='fixed right-3 bottom-2'>
-                <FaPhone className=' text-green-700 text-[30px]' title='Contact'/>
-            </div>
         </>
     )
 }
