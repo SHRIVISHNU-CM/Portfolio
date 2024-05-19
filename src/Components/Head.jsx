8e88fdf4a1a16d9f488b152e41e52317ec995b0f
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-05-19 16:06:04 +0530

    anchor tag

--- src/Components/Head.jsx
+++ src/Components/Head.jsx
@@ -3,7 +3,7 @@ import Pic from "../public/Vishnu.jpeg"
 function Head() {
     return (
         <>
-            <div className="flex justify-evenly items-center">
+            <div className="flex justify-evenly items-center my-5 text-white">
                 <div className="card w-[350px]">
                     <div className="card-body">
                         <h2 className="">
@@ -15,7 +15,7 @@ function Head() {
                         </h1>
                         <p>A <span className="text-orange-500 font-semibold">Full-Stack developer</span> passionate about moulding ideas into breathtaking digital experiences</p>
                         <div className="card-actions justify-end">
-                            <button className="btn btn-success text-white">My CV</button>
+                            <a href="https://drive.google.com/file/d/1ZajV39uOHf0p3SQXaHdThGBKwZ_-DYMc/view?usp=sharing" className="btn btn-success text-white">Resume</a>
                         </div>
 
 
