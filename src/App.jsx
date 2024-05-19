dc915ca36362be1a862d87effd149b61b013bce6
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-05-19 13:33:40 +0530

    skills,projects,education,footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import About from "./Components/About"
+import Education from "./Components/Education"
+import Footer from "./Components/Footer"
 import Head from "./Components/Head"
 import Navbar from "./Components/Navbar"
+import Projects from "./Components/Projects"
+import Skills from "./Components/Skills"
 
 function App() {
 
   return (
     <>
-      <div  className=" h-screen ">
-      <Navbar />
-      <Head/>
-      <About/>
+      <div className=" " style={{backgroundColor:"#002147"}}>
+        <Navbar />
+        <Head />
+        <About />
+        <Skills />
+        <Projects />
+        <Education/>
+        <Footer/>
 
       </div>
     </>
