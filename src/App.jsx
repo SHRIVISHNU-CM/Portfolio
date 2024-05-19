40d547ad9553fd0842287bb653a4887506b4f492
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-05-19 16:04:16 +0530

    Framer motion useScroll

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,29 @@ import Head from "./Components/Head"
 import Navbar from "./Components/Navbar"
 import Projects from "./Components/Projects"
 import Skills from "./Components/Skills"
+import { motion, useScroll } from "framer-motion"
 
 function App() {
-
+  const { scrollYProgress } = useScroll();
   return (
     <>
-      <div className=" " style={{backgroundColor:"#002147"}}>
-        <Navbar />
-        <Head />
-        <About />
-        <Skills />
-        <Projects />
-        <Education/>
-        <Footer/>
+        <div style={{ backgroundColor: "#002147" }}>
+        <motion.div className="fixed top-0 left-0 right-0 h-2  bg-red-500 origin-left" style={{scaleX:scrollYProgress}}/>
 
-      </div>
+          <Navbar />
+          <Head />
+          <About />
+          <Skills />
+          <Projects />
+          <Education />
+          <Footer />
+
+        </div>
+    
+
     </>
   )
 }
+import { FaRegMoneyBill1 } from "react-icons/fa6"
 
 export default App
