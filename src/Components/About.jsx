7b5c210dc8a6bf8b6b9391ab84d5ab83ee0b8ebc
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-05-19 13:26:53 +0530

    responsive and text color

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,12 +3,12 @@
 function About() {
     return (
         <>
-            <div className="flex justify-around items-center">
-                <div className=" shadow-lg">
-                    <h1 className="card-title text-[25px]">About ME</h1>
+            <div className="flex flex-col justify-around items-center lg:flex lg:flex-row my-5">
+                <div >
+                    <h1 className="text-[25px] font-semibold text-white">About ME</h1>
                 </div>
-                <div className="w-[300px]">
-                    <p className="">
+                <div className=" w-min h-min">
+                    <p className="text-white">
                         I'm skilled in HTML,CSS,Tailwind CSS,DaisyUI,Javascript,React.js,Node.js,Express.js,MongoDB,SQL,and Python
                     </p>
                 </div>
