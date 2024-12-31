4b5b9f4201f4ac86c53e5ecd6be2e42f20759b2f
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-12-31 10:25:56 +0530

    updated footer & git

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,6 +6,16 @@ function Footer() {
     return (
         <>
             <div className=" border-t-2 py-4 mt-3">
+                <div className="flex justify-center gap-x-9 mb-3">
+                <a href="https://github.com/SHRIVISHNU-CM">
+                            <BsGithub className="text-[24px] lg:text-[20px] text-gray-900" />
+
+                        </a>
+                        <a href="https://www.linkedin.com/in/shrivishnu-cm-070b78274/">
+                            <FaLinkedin className="text-[24px] lg:text-[20px] text-blue-800" />
+
+                        </a>
+                </div>
                 <h1 className="text-center text-[14px] lg:text-[16px] font-semibold">
                     Developed By Shri Vishnu C M
                 </h1>
--- src/Components/GitCard.jsx
+++ src/Components/GitCard.jsx
@@ -17,11 +17,17 @@ function GitCard({ props }) {
                 <div className=" w-full relative max-w-xs lg:max-w-xl">
                     <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
                     <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-around items-center">
-                        
+
                         <div>
-                            <h1 className="font-bold text-[16px] lg:text-xl text-white mb-4 relative z-50">
-                                My total commits {props}
-                            </h1>
+
+                            {
+                                props ?
+                                    <h1 className="font-bold text-[16px] lg:text-xl text-white mb-4 relative z-50">
+                                        My total commits {props}
+                                    </h1>
+                                    : <h1 className="font-bold text-[16px] lg:text-xl text-white mb-4 relative z-50"> GIT Status</h1>
+                            }
+
 
                             <p className="font-normal text-[16px] lg:text-xl text-white mb-4 relative z-50">
                                 USERNAME : {Data.login}
@@ -33,9 +39,9 @@ function GitCard({ props }) {
                                 Respositeries : {Data.public_repos}
                             </p>
                         </div>
-                        
 
-                        <img src={Data.avatar_url} style={{ width: "50px", borderRadius: "100%",zIndex:50 }} />
+
+                        <img src={Data.avatar_url} style={{ width: "50px", borderRadius: "100%", zIndex: 50 }} />
 
                         <Meteors number={50} />
                     </div>
