424cb24ef906d89fa598ade1cf407f5ef60aa468
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-12-31 10:14:03 +0530

    router-dom outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,24 @@
-import About from "./Components/About"
-import Education from "./Components/Education"
-import Feedback from "./Components/Feedback"
+
+import { Outlet } from "react-router-dom";
 import Footer from "./Components/Footer"
 import Head from "./Components/Head"
-import Navbar from "./Components/Navbar"
-import Projects from "./Components/Projects"
-import Skills from "./Components/Skills"
+import NavBar from "./Components/Navbar"
 import { motion, useScroll } from "framer-motion"
+
+
 
 function App() {
   const { scrollYProgress } = useScroll();
   return (
     <>
-      <div style={{ backgroundColor: "#002147", width:"100%"}} className="overflow-x-hidden">
-        <motion.div className="fixed top-0 left-0  h-2  rounded-xl bg-red-500 origin-left" style={{ scaleX: scrollYProgress }} />
-
-
-        <Navbar />
-        <Head />
-        <About />
-        <Skills />
-        <Projects />
-        <Education />
-        <Feedback/>
+      <div className="h-full w-full dark:bg-black bg-white dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2]">
+        <NavBar />
+        <Outlet />
         <Footer />
-
       </div>
-
-
     </>
   )
 }
-import { FaRegMoneyBill1 } from "react-icons/fa6"
+
 
 export default App
