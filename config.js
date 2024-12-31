cac86139118b17cc84423c9cb207f74d665ec6c2
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-12-31 10:06:04 +0530

    email.js config & token git

---
+++ config.js
@@ -0,0 +1,4 @@
+export const VITE_TOKEN = import.meta.env.VITE_TOKEN
+export const VITE_SERVICES = import.meta.env.VITE_SERVICES
+export const VITE_TEMPLETE = import.meta.env.VITE_TEMPLETE
+export const VITE_PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY
