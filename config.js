cd403e3eb917147c20a5660a117aca596a03122f
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-12-31 15:39:14 +0530

    resume

--- config.js
+++ config.js
@@ -2,3 +2,4 @@ export const VITE_TOKEN = import.meta.env.VITE_TOKEN
 export const VITE_SERVICES = import.meta.env.VITE_SERVICES
 export const VITE_TEMPLETE = import.meta.env.VITE_TEMPLETE
 export const VITE_PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY
+export const VITE_RESUME = import.meta.env.VITE_RESUME
