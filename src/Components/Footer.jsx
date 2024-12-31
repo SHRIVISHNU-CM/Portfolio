a2e670e0f124d62bc084c1a0658644098c7f891a
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-12-31 10:11:59 +0530

    simple footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,39 +5,10 @@ import { motion } from "framer-motion"
 function Footer() {
     return (
         <>
-            <div className="my-t-5 max-w-full h-[50vh] flex items-center flex-col justify-center ">
-                <motion.h1
-                    whileInView={{ opacity: 1, y: 0 }}
-                    initial={{ opacity: 0, y: -50 }}
-                    transition={{ duration: 1 }}
-
-                    className="text-center text-[30px] text-white">Connect with Me on</motion.h1>
-                <div className="flex justify-center gap-5">
-                    <motion.a
-                        whileInView={{ opacity: 1, y: 0 }}
-                        initial={{ opacity: 0, y: -50 }}
-                        transition={{ duration: 1}}
-                        href="https://github.com/SHRIVISHNU-CM">
-                        <BsGithub className="text-[45px] text-white" />
-
-                    </motion.a>
-                    <motion.a
-                        whileInView={{ opacity: 1, y: 0 }}
-                        initial={{ opacity: 0, y: -50 }}
-                        transition={{ duration: 2 }}
-                        href="https://www.linkedin.com/in/shrivishnu-cm-070b78274/">
-                        <FaLinkedin className="text-[45px] text-blue-800" />
-
-                    </motion.a>
-                    <motion.a
-                        whileInView={{ opacity: 1, y: 0 }}
-                        initial={{ opacity: 0, y: -50 }}
-                        transition={{ duration: 3 }}
-                        href="https://discord.com/1032526474495926362">
-                        <FaDiscord className="text-[45px] text-purple-800" />
-                    </motion.a>
-                </div>
-
+            <div className=" border-t-2 py-4 mt-3">
+                <h1 className="text-center text-[14px] lg:text-[16px] font-semibold">
+                    Developed By Shri Vishnu C M
+                </h1>
             </div>
 
         </>
