92ac770e60f7bd71cb201aac114d6f33fb0f2018
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-12-31 10:14:21 +0530

    routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Experience from './Components/Experience.jsx'
+import Projects from './Components/Projects.jsx'
+import Feedback from './Components/Feedback.jsx'
+import Head from './Components/Head.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route index element={<Head/>} />
+          <Route path='experience' element={<Experience />} />
+          <Route path='projects' element={<Projects />} />
+          <Route path='contact' element={<Feedback />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
   </React.StrictMode>,
 )
