727b4fcdf3c1599c2c839d8756257e876517d27c
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-12-31 10:13:43 +0530

    responsive skills

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,148 +1,54 @@
+import React from "react";
 import { FaHtml5 } from "react-icons/fa";
 import { IoLogoCss3 } from "react-icons/io";
 import { IoLogoJavascript } from "react-icons/io5";
 import { RiTailwindCssFill } from "react-icons/ri";
-import { SiDaisyui } from "react-icons/si";
+import { SiDaisyui, SiExpress, SiMongodb, SiMysql, SiPostman, SiFramer } from "react-icons/si";
 import { DiNodejs } from "react-icons/di";
 import { RiReactjsLine } from "react-icons/ri";
-import { SiExpress } from "react-icons/si";
-import { SiMongodb } from "react-icons/si";
-import { SiMysql } from "react-icons/si";
 import { FaPython } from "react-icons/fa";
-import { SiPostman } from "react-icons/si";
 import { motion } from "framer-motion";
-import { SiAutocad } from "react-icons/si";
-import { SiFramer } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+import { FaDocker } from "react-icons/fa"
+
 function Skills() {
-    const icon = (dur) => ({
-        inital: { y: -10 },
-        animate: {
-            y: [2, -2],
-            x: [2, -1],
-            transition: {
-                duration: dur,
-                ease: "linear",
-                repeat: Infinity,
-                repeatType: "reverse"
-            }
 
-        }
-    })
+    const skills = [
+        { name: "HTML5", icon: <FaHtml5 />, color: "orange" },
+        { name: "CSS3", icon: <IoLogoCss3 />, color: "oxxfordblue" },
+        { name: "JavaScript", icon: <IoLogoJavascript />, color: "yellow" },
+        { name: "Tailwind CSS", icon: <RiTailwindCssFill />, color: "teal" },
+        { name: "DaisyUI", icon: <SiDaisyui />, color: "purple" },
+        { name: "Node.js", icon: <DiNodejs />, color: "green" },
+        { name: "React", icon: <RiReactjsLine />, color: "cyan" },
+        { name: "Express", icon: <SiExpress />, color: "gray" },
+        { name: "MongoDB", icon: <SiMongodb />, color: "green" },
+        { name: "MySQL", icon: <SiMysql />, color: "blue" },
+        { name: "Python", icon: <FaPython />, color: "blue" },
+        { name: "Postman", icon: <SiPostman />, color: "orange" },
+        { name: "Framer", icon: <SiFramer />, color: "pink" },
+        { name: "Github", icon: <FaGithub />, color: "black" },
+        { name: "Docker", icon: <FaDocker />, color: "blue" },
+    ];
+
     return (
-        <>
-            <div className="h-[60vh] max-w-full flex items-center flex-col gap-y-6">
-                <h1 className="text-center text-[30px] font-semibold text-white">Skills</h1>
-
-                <section className="flex justify-center gap-3 flex-wrap text-xl lg:text-2xl">
-
-                    <motion.div
-                        variants={icon(2.9)}
-                        initial="inital"
-                        animate="animate"
-                        className="border w-min p-3 rounded-lg ">
-                        <FaHtml5 className="text-orange-400 text-[50px] " />
-                    </motion.div>
-                    <motion.div
-                        variants={icon(1.5)}
-                        initial="initial"
-                        animate="animate"
-                        className="border w-min p-3 rounded-lg">
-                        <IoLogoCss3 className="text-blue-400 text-[50px]" />
-                    </motion.div>
-                    <motion.div
-                        variants={icon(2.9)}
-                        initial="initial"
-                        animate="animate"
-                        className="border w-min p-3 rounded-lg">
-                        <IoLogoJavascript className="text-orange-400 text-[50px]" />
-                    </motion.div>
-                    <motion.div
-                        variants={icon(1.5)}
-                        initial="initial"
-                        animate="animate"
-                        className="border w-min p-3 rounded-lg">
-                        <RiTailwindCssFill className="text-blue-400 text-[50px]" />
-                    </motion.div>
-                    <motion.div
-                        variants={icon(2.9)}
-                        initial="initial"
-                        animate="animate"
-                        className="border w-min p-3 rounded-lg">
-                        <SiDaisyui className="text-neutral-600 text-[50px]" />
-                    </motion.div>
-                    <motion.div
-                        variants={icon(1.5)}
-                        initial="initial"
-                        animate="animate"
-                        className="border w-min p-3 rounded-lg">
-                        <DiNodejs className="text-green-600 text-[50px]" />
-                    </motion.div>
-
-
-
-                    <motion.div
-                        variants={icon(2.9)}
-                        initial="initial"
-                        animate="animate"
-                        className="border w-min p-3 rounded-lg">
-                        <RiReactjsLine className=" text-[50px] text-blue-400" />
-                    </motion.div>
-                    <motion.div
-                        variants={icon(1.5)}
-                        initial="initial"
-                        animate="animate"
-                        className="border w-min p-3 rounded-lg">
-                        <SiExpress className="text-neutral-600 text-[50px]" />
-                    </motion.div>
-                    <motion.div
-                        variants={icon(2.9)}
-                        initial="initial"
-                        animate="animate"
-                        className="border w-min p-3 rounded-lg">
-                        <SiMongodb className=" text-[50px] text-green-500" />
-                    </motion.div>
-                    <motion.div
-                        variants={icon(1.5)}
-                        initial="initial"
-                        animate="animate"
-                        className="border w-min p-3 rounded-lg">
-                        <SiMysql className=" text-[50px] text-blue-800" />
-                    </motion.div>
-                    <motion.div
-                        variants={icon(2.9)}
-                        initial="initial"
-                        animate="animate"
-                        className="border w-min p-3 rounded-lg">
-                        <FaPython className=" text-[50px] text-yellow-400" />
-                    </motion.div>
-                    <motion.div
-                        variants={icon(1.5)}
-                        initial="initial"
-                        animate="animate"
-                        className="border w-min p-3 rounded-lg">
-                        <SiPostman className=" text-[50px] text-orange-400" />
-                    </motion.div>
-                    <motion.div
-                        variants={icon(1.5)}
-                        initial="initial"
-                        animate="animate"
-                        className="border w-min p-3 rounded-lg">
-                        <SiAutocad className=" text-[50px] text-red-400" />
-                    </motion.div>
-                    <motion.div
-                        variants={icon(1.5)}
-                        initial="initial"
-                        animate="animate"
-                        className="border w-min p-3 rounded-lg">
-                        <SiFramer className=" text-[50px] text-slate-600" />
-                    </motion.div>
-
-                </section>
+        <div>
+            <div>
+                <h1 className="text-center font-semibold text-[16px] lg:text-[24px]">Skills</h1>
             </div>
-
-
-        </>
-    )
+            <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center lg:w-[500px] mx-auto mt-4">
+                {skills.map((skill, index) => (
+                    <motion.div
+                        key={index}
+                        className="flex flex-col items-center text-center p-4 bg-slate-200 rounded-lg hover:scale-105 transition-transform"
+                    >
+                        <div className={`text-4xl `} style={{color:skill.color}}>{skill.icon}</div>
+                        
+                    </motion.div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default Skills
+export default Skills;
